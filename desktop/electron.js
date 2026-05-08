e34f5fa7aa0d4645475c517302a0c1b1195408a6
REZGA <rezigabderahmane@gmail.com>
2026-05-08 20:01:08 +0300

    First step for my app

---
+++ desktop/electron.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
+    icon: path.join(__dirname, '../frontend/public/panlelogo.svg'),
+    show: false
+  });
+
+  const isDev = !app.isPackaged;
+  
+  if (isDev) {
+    // Development: load from Vite dev server
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    // Production: load built files
+    mainWindow.loadFile(path.join(__dirname, '../frontend/dist/index.html'));
+  }
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
